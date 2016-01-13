416f961110fbe70a3391a4a324493d4ec19c2ea2
karenjho <hokarenj@gmail.com>
2016-01-13 13:49:38 -0500

    Output result of temperature converter to user

--- exercise5.rb
+++ exercise5.rb
@@ -6,3 +6,7 @@ def temp_converter(fahrenheit)
   result = (fahrenheit - 32) * 5 / 9
   result
 end
+
+celsius = temp_converter(fahrenheit)
+
+puts "#{fahrenheit.to_s} degrees Fahrenheit is equal to #{celsius.to_s} degrees Celsius."
