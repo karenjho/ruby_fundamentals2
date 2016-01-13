8132291acb8bbf936268f2181c1d48d1eb636f5b
karenjho <hokarenj@gmail.com>
2016-01-13 14:24:41 -0500

    Add rice to grocery list and output again

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,6 @@
+# grocery list step 1: print the list
+puts "Grocery List: Step One".upcase
+
 grocery_list = [
   "chickpeas",
   "arugula",
@@ -7,6 +10,18 @@ grocery_list = [
 ]
 
 puts grocery_list
+
+# grocery list step 2: add asterisks for clarity
+puts "Grocery List: Step Two".upcase
+
+grocery_list.each do |item|
+  puts "* #{item}"
+end
+
+# grocery list step 3: add rice to the list and print again
+puts "Grocery List: Step Three".upcase
+
+grocery_list << "rice"
 
 grocery_list.each do |item|
   puts "* #{item}"
