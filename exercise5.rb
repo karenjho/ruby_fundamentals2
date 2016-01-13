94ce70a114d9cfdc6085fefc5c7882f3ddf77fec
karenjho <hokarenj@gmail.com>
2016-01-13 13:47:19 -0500

    Add temp_converter method

--- exercise5.rb
+++ exercise5.rb
@@ -1,3 +1,8 @@
 puts "Welcome to the Fahrenheit-to-Celsius converter."
 puts "Provide a temperature to convert from F to C (numberical values only):"
 fahrenheit = gets.chomp.to_i
+
+def temp_converter(fahrenheit)
+  result = (fahrenheit - 32) * 5 / 9
+  result
+end
